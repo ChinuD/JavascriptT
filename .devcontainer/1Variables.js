e28a7e99935003d76857c1af4262bc3c40aef827
Chinmay Dhokey <148992928+ChinuD@users.noreply.github.com>
2024-09-18 13:07:16 +0530

    a

--- .devcontainer/1Variables.js
+++ .devcontainer/1Variables.js
@@ -1,1 +1,7 @@
-console.log("Chinmay")
+const accountId = 14433; //Change nahi kar sakte
+let accountEmail = "hitesh@google.com"; 
+var accountPassword = "12345";
+accountCity = "Pune";
+
+accountId = 2;
+console.log(accountId);
